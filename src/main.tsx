8fd511577dd9462bbb6682368795ea1e7e32470e
Roman <43324830+romanMamrukov@users.noreply.github.com>
2026-09-02 14:17:42 +0300

    Add src/main.tsx

---
+++ src/main.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Home from "../app/page";
+import "../app/globals.css";
+import "../app/panels.css";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(<React.StrictMode><Home/></React.StrictMode>);
